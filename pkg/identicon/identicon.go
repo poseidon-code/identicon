3573c641f92ba24515336ae18bc2c8b7d1d826fb
poseidon-code <pritamhalder05062000@gmail.com>
2022-02-04 11:56:28 +0530

    refactor: ordering of program exiting block at 1st

--- pkg/identicon/identicon.go
+++ pkg/identicon/identicon.go
@@ -26,19 +26,19 @@ type Identicon struct {
 
 // creates and sets all the fields of Identicon struct
 func (i *Identicon) New() {
-    // handling type (square|wide)
-    if i.Options.Square {
-        i.Hash, i.Width, i.Height = h.SHA256(i.Text)
-    } else {
-        i.Hash, i.Width, i.Height = h.SHA512(i.Text)
-    }
-
-
     // handling size (4-8)
     if i.Options.Size<4 || i.Options.Size>8 {
         fmt.Println("Invalid size passed. \nSize must lie between 4 to 8 (inclusive).")
         os.Exit(1)
     }
+
+
+    // handling type (square|wide)
+    if i.Options.Square {
+        i.Hash, i.Width, i.Height = h.SHA256(i.Text)
+    } else {
+        i.Hash, i.Width, i.Height = h.SHA512(i.Text)
+    }    
 
 
     // handling vertical dimension (rather than rotating the entire martrix, only the dimensions are switched) (landscape|portrait)
